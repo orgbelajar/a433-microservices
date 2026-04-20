f0991d11558b295cbf5f73ba624ccfc5e5c96f96
Nabil <nabilsyakir95@gmail.com>
2026-04-20 10:27:06 +0700

    feat: implement CI pipeline with Docker linting, unit testing, and automated image deployment to GHCR

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Menginisialisasi variabel untuk nama image, tag image, dan username GitHub
+image_name="karsajobs"
+image_tag="latest"
+github_username="orgbelajar"
+github_package_name="$github_username/$image_name:$image_tag"
+
+# Perintah untuk build Docker image dari berkas Dockerfile
+docker build . -t $github_package_name
+
+# Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag $github_package_name ghcr.io/$github_package_name
+
+# Perintah untuk login ke GitHub Package
+echo $KARSAJOBS_PAT | docker login ghcr.io -u $github_username --password-stdin
+
+# Perintah untuk push image ke Github Package
+docker push ghcr.io/$github_package_name
